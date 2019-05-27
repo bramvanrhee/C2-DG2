5b5314579e81bf04130c4495edaf60530cbe2375
AndyLammerts <124131@student.drenthecollege.nl>
2019-05-27 11:02:05 +0200

    Keuze man/vrouw verandert style van de site

--- Opdracht 4 (Andy)/Javascript/opdracht4.js
+++ Opdracht 4 (Andy)/Javascript/opdracht4.js
@@ -1,31 +1,46 @@
-//Verschillende namen voor de Array
+//Verschillende namen voor de Array (Zodat dat goed op het formulier kan staan)
 var veldenNed = ["Naam:","Adres:","Woonplaats:", "Postcode:", "Geboortedatum:", "Nationaliteit:", "Beroep:", "Opmerkingen: "];
 var veldenEng = ["Name:","Address:","Residence:","Postcode:","Date of Birth:","Nationality:","Work:", "Comments: "];
 //Functie om leeftijd te berekenen
-function leeftijd (dateString) {
-  var vandaag = new Date();
-  var geboren = new Date (dateString);
-  var jaar = vandaag.getFullYear() - geboren.getFullYear();
-  var maand = vandaag.getMonth() - geboren.getMonth();
-  if (maand < 0 || (maand === 0 && vandaag.getDate() < geboren.getDate())) {
-          jaar--;
-      }
-      return jaar;
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
+
 //Kiezen voor Nederlands of Engels
 $(document).ready(function(){
   $("#nederlands").click(function(){
     $("#kiesTaal").fadeOut("slow");
     $("#ned").fadeIn("slow");
     $("td.nedtext").each(function(i,ind){
-      $(`#${ind.id}`).text(nedvelden[i]);
+      $(`#${ind.id}`).text(veldenNed[i]);
     });
   });
   $("#engels").click(function(){
     $("#kiesTaal").fadeOut("slow");
     $("#eng").fadeIn("slow");
     $("td.engtext").each(function(i,ind){
-      $(`#${ind.id}`).text(engvelden[i]);
+      $(`#${ind.id}`).text(veldenEng[i]);
     });
   });
 });
+
+//Kiezen tussen man en vrouw
+$(".male").click(function(){
+    $("body").css("background-color","green");
+    $("input[type='text']").css("background-color","LightGreen");
+    $("input[type='date']").css("background-color","LightGreen");
+    $("textarea").css("background-color","LightGreen");
+  });
+  $(".female").click(function(){
+    $("body").css("background-color","purple");
+    $("input[type='text']").css("background-color","pink");
+    $("input[type='date']").css("background-color","pink");
+    $("textarea").css("background-color","pink");
+  });
