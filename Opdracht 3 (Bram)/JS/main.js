ffd548173d6aba2f43c5e252c745b1397105e27f
Bram van Rhee <bramvanrhee01@gmail.com>
2019-06-02 12:40:37 +0200

    Javascript om de namen rechts te krijgen

--- Opdracht 3 (Bram)/JS/main.js
+++ Opdracht 3 (Bram)/JS/main.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
       document.getElementById('b1').innerHTML = linksarray.join("");
     }
   });
+
+  $('#rechts').click(function() {
+    rechtsarray.push(linksarray.join(""));
+    rechtsarray.join("");
+    linksarray = [];
+    document.getElementById('b1').innerHTML = linksarray.join("");
+    document.getElementById('b2').innerHTML = rechtsarray;
+  });
 });
